2d0cf6fc2a2ccdeb1c68d70eae187dffd770e70b
jafarxd <jafaralsalehi@gmail.com>
2025-01-26 23:25:01 -0800

    test

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./mySpriteSheet_cropped.png");
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+	gameEngine.addEntity(new mySkeleton(gameEngine));
 
 	gameEngine.init(ctx);
 
--- mySkeleton.js
+++ mySkeleton.js
@@ -1,14 +1,24 @@
-const gameEngine = new GameEngine();
+class mySkeleton {
+	constructor(game){
+		this.game = game;
+		this.animator = new Animator(ASSET_MANAGER.getAsset("./mySpriteSheet_cropped.png"), 3, 0, 48.0059, 75, 9, 0.15)
+		this.x = 0;
+		this.y = 0;
+		this.speed = 100;
+	};
 
-const ASSET_MANAGER = new AssetManager();
+	update() {
+			this.x += this.speed*this.game.clockTick;
+			if(this.x > 1024) this.x = 0;
+	};
 
-ASSET_MANAGER.queueDownload("./mySpriteSheet.png");
+	draw(ctx){
+		this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+	};
 
-ASSET_MANAGER.downloadAll(() => {
-	const canvas = document.getElementById("gameWorld");
-	const ctx = canvas.getContext("2d");
 
-	gameEngine.init(ctx);
 
-	gameEngine.start();
-});
+
+
+
+}
